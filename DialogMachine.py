ad49488e8d3a1b493d9e72999a57bc0c8e537233
Mikhail <misha.kolmykovqq1@yandex.ru>
2021-12-10 00:12:25 +0300

    1

---
+++ DialogMachine.py
@@ -0,0 +1,30 @@
+from transitions import Machine
+
+
+class DialogMachine(object):
+    _states = ['ready for start', 'waiting for size', 'waiting for payment method',
+              'waiting for confirmation', 'confirm order']
+    _pizza_size = ''
+    _payment_method = ''
+
+    def __init__(self, id_):
+        self._machine = Machine(model=self, states=DialogMachine._states, initial='ready for start')
+
+        self._machine.add_transition('start_dialog', 'ready for start', 'waiting for size')
+        self._machine.add_transition('accept_size', 'waiting for size', 'waiting for payment method')
+        self._machine.add_transition('accept_pay_method', 'waiting for payment method', 'waiting for confirmation')
+        self._machine.add_transition('confirm_order', 'waiting for confirmation', 'confirm order')
+
+        self._machine.add_transition('start_again', '*', 'ready for start',
+                                     before='clear_data')
+        self._machine.add_transition('back_from_pay', 'waiting for payment method', 'waiting for size',
+                                     before='clear_payment_method')
+        self._machine.add_transition('back_from_confirm', 'waiting for confirmation', 'waiting for payment method',
+                                     before='clear_payment_method')
+
+    def clear_data(self):
+        self._pizza_size = ''
+        self._payment_method = ''
+
+    def clear_payment_method(self):
+        self._payment_method = ''
--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-from transitions import Machine
+from DialogMachine import DialogMachine
 
+dMachine = DialogMachine(123)
+print(dMachine.state)
+dMachine.start_dialog()
+print(dMachine.state)
 
-class DialogMachine(object):
-    states = ['ready for start', 'waiting for size', 'waiting for payment method',
-              'waiting for confirmation', 'confirm order']
-    pizza_size = ''
-    payment_method = ''
-
-    def __init__(self, id):
-        self.machine = Machine(model=self, states=DialogMachine.states, initial='ready for start')
-
-        self.machine.add_transition('start dialog', 'ready for start', 'waiting for size')
-        self.machine.add_transition('accept size', 'waiting for size', 'waiting for payment method')
-        self.machine.add_transition('accept pay method', 'waiting for payment method', 'waiting for confirmation')
-        self.machine.add_transition('confirm order', 'waiting for confirmation', 'confirm order')
-
-        self.machine.add_transition('start again', '*', 'ready for start',
-                                    before='clear_data')
-        self.machine.add_transition('back from pay', 'waiting for payment method', 'waiting for size',
-                                    before='clear_payment_method')
-        self.machine.add_transition('back from ', 'waiting for confirmation', 'waiting for payment method',
-                                    before='clear_payment_method')
-
-    def clear_data(self):
-        self.pizza_size = ''
-        self.payment_method = ''
-
-    def clear_payment_method(self):
-        self.payment_method = ''
